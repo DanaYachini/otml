bb615d90664955a94545344821a88150df9d61f2
iddober <iddoberger@gmail.com>
2024-03-03 19:04:36 +0200

    move stuff to old

--- source/new_main.py
+++ source/new_main.py
@@ -42,22 +42,29 @@ console_handler.setFormatter(file_log_formatter)
 logger.addHandler(console_handler)
 
 
+feature_table_file_stem = "a_b_and_son_feature_table"
+corpus_file_stem = "small_ab_target_corpus"
+constraint_set_file_stem = "faith_constraint_set"
 
-feature_table_file_path = str(Path(fixtures_directory_path, "feature_table", "a_b_and_son_feature_table.json"))
-corpus_file_path = str(Path(fixtures_directory_path, "corpora", "bb_corpus.txt"))
-constraint_set_file_path = str(Path(fixtures_directory_path, "constraint_sets", "faith_constraint_set.json"))
+feature_table_file_path = str(Path(fixtures_directory_path, "feature_table", f"{feature_table_file_stem}.json"))
+corpus_file_path = str(Path(fixtures_directory_path, "corpora", f"{corpus_file_stem}.txt"))
+constraint_set_file_path = str(Path(fixtures_directory_path, "constraint_sets", f"{constraint_set_file_stem}.json"))
 
 
 configuration_json_str = codecs.open(configuration_file_path, 'r').read()
 OtmlConfigurationManager(configuration_json_str)
-
 
 feature_table = FeatureTable.load(feature_table_file_path)
 corpus = Corpus.load(corpus_file_path)
 constraint_set = ConstraintSet.load(constraint_set_file_path, feature_table)
 lexicon = Lexicon(corpus.get_words(), feature_table)
 grammar = Grammar(feature_table, constraint_set, lexicon)
-data = corpus.get_words()
-traversable_hypothesis = TraversableGrammarHypothesis(grammar, data)
-simulated_annealing = SimulatedAnnealing(traversable_hypothesis)
-simulated_annealing.run()
+#data = corpus.get_words()
+
+print(grammar.get_all_outputs_grammar())
+
+print(data)
+
+# traversable_hypothesis = TraversableGrammarHypothesis(grammar, data)
+# simulated_annealing = SimulatedAnnealing(traversable_hypothesis)
+# simulated_annealing.run()
