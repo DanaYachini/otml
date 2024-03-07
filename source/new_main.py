567d97d0b7fa038388f66b4077c330bab4bc886f
iddober <iddoberger@gmail.com>
2024-03-07 20:39:26 +0200

    add bb_only_delete_segment_configuration.json

--- source/new_main.py
+++ source/new_main.py
@@ -1,16 +1,22 @@
 import codecs
 from logging import StreamHandler
-
 import logging
 from otml_configuration_manager import OtmlConfigurationManager
 from pathlib import Path
-
 
 current_file_path = Path(__file__)
 source_directory_path = current_file_path.parent
 fixtures_directory_path = Path(source_directory_path, "tests", "fixtures")
 
-configuration_file_path = str(Path(fixtures_directory_path, "configuration", "bb_demote_only_configuration.json"))
+
+RUN_NAME = "bb_only_delete_segment_configuration__2024_03_07"
+FEATURE_TABLE_FILE_NAME = "a_b_and_cons_feature_table.json"
+CORPUS_FILE_NAME = "bb_corpus.txt"
+CONSTRAINT_SET_FILE_NAME = "bb_target_constraint_set.json"
+CONFIGURATION_FILE_NAME = "bb_only_delete_segment_configuration.json"
+
+
+configuration_file_path = str(Path(fixtures_directory_path, "configuration", CONFIGURATION_FILE_NAME))
 
 configuration_json_str = codecs.open(configuration_file_path, 'r').read()
 OtmlConfigurationManager(configuration_json_str)
@@ -31,7 +37,7 @@ log_file_path = str(Path(logging_directory_path, "ab.txt"))
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-file_log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s', "%Y-%m-%d %H:%M:%S")
+file_log_formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s", "%Y-%m-%d %H:%M:%S")
 file_log_handler = logging.FileHandler(log_file_path, mode='w')
 file_log_handler.setFormatter(file_log_formatter)
 logger.addHandler(file_log_handler)
@@ -42,24 +48,9 @@ console_handler.setFormatter(file_log_formatter)
 logger.addHandler(console_handler)
 
 
-feature_table_file_name = "a_b_and_cons_feature_table.json"
-
-# corpus_file_name = "bb_corpus.txt"
-# constraint_set_file_name = "faith_constraint_set.json"
-
-
-# 9230
-
-# 412,430
-
-corpus_file_name = "bb_target_lexicon.txt"
-constraint_set_file_name = "bb_target_constraint_set.json"
-#
-# # 411,582
-
-feature_table_file_path = str(Path(fixtures_directory_path, "feature_table", feature_table_file_name))
-corpus_file_path = str(Path(fixtures_directory_path, "corpora", corpus_file_name))
-constraint_set_file_path = str(Path(fixtures_directory_path, "constraint_sets", constraint_set_file_name))
+feature_table_file_path = str(Path(fixtures_directory_path, "feature_table", FEATURE_TABLE_FILE_NAME))
+corpus_file_path = str(Path(fixtures_directory_path, "corpora", CORPUS_FILE_NAME))
+constraint_set_file_path = str(Path(fixtures_directory_path, "constraint_sets", CONSTRAINT_SET_FILE_NAME))
 
 
 configuration_json_str = codecs.open(configuration_file_path, 'r').read()
@@ -82,5 +73,10 @@ traversable_hypothesis = TraversableGrammarHypothesis(grammar, data)
 
 print(traversable_hypothesis.get_energy())
 
-# simulated_annealing = SimulatedAnnealing(traversable_hypothesis)
-# simulated_annealing.run()
+simulated_annealing = SimulatedAnnealing(traversable_hypothesis)
+simulated_annealing.run()
+
+# 412,494 bits (Grammar = 9,294) + (Data = 403,200)
+
+
+# 407,274 bits (Grammar = 4,074) + (Data = 403,200)
